d4ccec3b7db29b23696ce262d6389c69e969ef04
De Wulf Jerome <dewulje@cronos.be>
2020-04-03 15:56:39 +0200

    added buttonpress animation

--- components/buttons/Button.js
+++ components/buttons/Button.js
@@ -1,25 +1,67 @@
-import React from 'react';
-import {TouchableOpacity, Text, StyleSheet} from 'react-native';
+import React, {useEffect, useRef} from 'react';
+import {
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Text,
+  View,
+  StyleSheet,
+  Animated,
+} from 'react-native';
 
 import StyleConstants from '../../StyleConstants';
 
 const Button = props => {
+  const pressAnim = useRef(new Animated.Value(0)).current;
+
+  const animateButtonPress = direction => {
+    Animated.timing(pressAnim, {
+      toValue: direction,
+      duration: 100,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const scale = pressAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: [1, 0.97],
+    extrapolate: 'clamp',
+  });
+
+  const opac = pressAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: [1, 0.65],
+    extrapolate: 'clamp',
+  });
+
   return (
-    <TouchableOpacity
-      style={[
-        buttonStyle.buttonWrapper,
-        props.disabled && buttonStyle.disabledWrapper,
-      ]}
+    <TouchableWithoutFeedback
+      style={[buttonStyle.buttonOuterWrapper]}
       disabled={props.disabled && props.disabled}
+      onPressIn={() => {
+        animateButtonPress(1);
+      }}
+      onPressOut={() => {
+        animateButtonPress(0);
+      }}
       onPress={() => {
         props.onPress();
       }}>
-      <Text style={buttonStyle.textStyle}>{props.text}</Text>
-    </TouchableOpacity>
+      <Animated.View
+        style={[
+          buttonStyle.buttonWrapper,
+          props.disabled && buttonStyle.disabledWrapper,
+          {transform: [{scale: scale}], opacity: opac},
+        ]}>
+        <Text style={buttonStyle.textStyle}>{props.text}</Text>
+      </Animated.View>
+    </TouchableWithoutFeedback>
   );
 };
 
 const buttonStyle = StyleSheet.create({
+  buttonOuterWrapper: {
+    width: '100%',
+  },
   buttonWrapper: {
     backgroundColor: StyleConstants.colors.blue.medium,
     justifyContent: 'center',
